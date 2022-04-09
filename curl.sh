f4681da25f6430f1583b30232918d348fd4267cd
textworld <text.zwb@gmail.com>
2022-04-09 12:32:58 +0800

    feat: update

--- curl.sh
+++ curl.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 
-echo "success"
-echo "error" >&2
+if [ -z $URL ];then
+  echo "env: url, can not be empty"
+  exit 1
+fi
+
+if [ -z $AUTH ];then
+  echo "env: auth, can not be empty"
+  exit 1
+fi
+
+curl -X POST "http://$URL/post" -H "Authorization: Basic $AUTH"
